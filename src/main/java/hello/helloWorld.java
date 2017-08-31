1128c49510f9ce78cd9ee40f03517a481fcf3d1e
training_B8C.03.01 <training_B8C.03.01@accenture.com>
2017-08-31 12:45:13 +0530

    Done

--- src/main/java/hello/helloWorld.java
+++ src/main/java/hello/helloWorld.java
@@ -2,10 +2,7 @@ package hello;
 
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
-		System.out.println("Added newly");
-		System.out.println("Added newly");
-		System.out.println("Added newly");
-                System.out.println("Added newly");
+        System.out.println("HelloWorld1");
+
     }
 }
